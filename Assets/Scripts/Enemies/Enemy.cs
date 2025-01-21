bcf5aefb2f1675485d93f11d363751cfd0d8699b
Francesco Cambi <francesco100@hotmail.it>
2025-01-21 13:34:39 +0100

    WIP: feat: add Enemy follow behavior

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private Vector2 distanceToPlayer = new Vector2(-1, -1);
+    [SerializeField] private float speed = 1f;
+
+    private Vector2 playerPosition => PlayerController.Instance.transform.position;
+    private Vector2 endPosition => playerPosition + distanceToPlayer;
+
+    private void Update() {
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Move() {
+        float step = speed * Time.deltaTime;
+        this.transform.position = Vector2.MoveTowards(this.transform.position, endPosition, step);
     }
 }
